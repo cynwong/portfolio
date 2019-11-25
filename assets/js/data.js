f1f301e2ccad9a6c3d1ebc153797fae4f87fa19b
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-25 23:47:19 +1100

    add  data

---
+++ assets/js/data.js
@@ -0,0 +1,11 @@
+const DATA = [
+    {
+        name : "Weather Dashboad",
+        imgURL: "https://raw.githubusercontent.com/cynwong/Weather_Dashboard/master/assets/images/weather_dashboard.png?token=AKKMXC3ZXCSF2NJQ6YFS7UC526OYQ",
+        githubURL: "https://github.com/cynwong/Weather_Dashboard",
+        deployedURL: "https://cynwong.github.io/Weather_Dashboard/",
+        description: "Weather dashboard is an one-page application where the user can search for the weather information by the name of a city.",
+        tags: ["HTML", "HTML5", "CSS", "Bootstrap", "Font-awesome", "JavaScript", "jQuery", "AJAX", "Promise", "OpenWeatherMap API", "API", "LocalStorage", "moment.js"]
+
+    }
+]
