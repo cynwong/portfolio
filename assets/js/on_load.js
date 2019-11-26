fbe16fc344556e36c8ab21c76ca272d142bb7c9c
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-26 23:04:36 +1100

    modify on_load file to add tab listeners and portfolio renderer

--- assets/js/on_load.js
+++ assets/js/on_load.js
@@ -1,46 +1,84 @@
+"use strict";
 
-$(document).ready(function(){
+const hideAllSections = function () {
+    $("main section").hide();
+};
 
-    const elements = [];
-    const tags = [];
-    const $container = $(".container");
-    const $tagCloud = $(".tag-cloud");
-    const tagElements = [];
+const changePage = function (toPage) {
+    // First, hide all sections
+    hideAllSections();
 
-    for(let d of DATA){
-        // get tag data for tag cloud
-        // for(let tag of d.tags){
-        //     if(tags.indexOf(tag) === -1){
-        //         tags.push(tag);
-        //     }
-        // }
-        // project display
-        const $newFigure = $container.find(".template").clone().removeClass("template");
-
-        $newFigure.find("img").attr({
-            src: d.imgURL,
-            alt: d.name
-        });
-
-        $newFigure.find(".img-label").text(d.name);
-        elements.push($newFigure);
+    // portfolio page is special. it must be generated from data. 
+    if (toPage.localeCompare("portfolio") === 0) {
+        generatePortfolio();
     }
 
-    // let count = 1;
-    // for (let tag of tags){
-    //     const li = $tagCloud.find(".tag-item.template").clone().removeClass("template");
-    //     li.find("a").addClass("size"+count).text(tag);
-    //     tagElements.push(li);
-    //     count++;
-    //     if(count > 6){
-    //         count = 1;
-    //     }
-    // }
-    //clear the containers
-    $container.empty();
-    // $tagCloud.empty();
+    // display chosen section
+    $("#" + toPage + "-section").show();
 
-    //add elements to appropriate containers
-    // $tagCloud.append(tagElements);
-    $container.append(elements);
+};
+
+const generatePortfolio = function () {
+
+    const $section = $("main #portfolio-section");
+    const $container = $section.find("#projects-container");
+    const projects = [];
+
+    //clean the container
+    $container.empty();
+
+    //prepare the card for user display
+    for (let project of PORTFOLIO_DATA) {
+        const {
+            name,
+            imgLogo,
+            imgURL,
+            deployedURL: url,
+            description,
+            tags: tagNames
+        } = project;
+        const $card = $section.find(".card.template").clone().removeClass("template");
+        const $img = $card.find(".card-figure img");
+        $img.attr("src", imgURL);
+        $img.attr("alt", imgLogo);
+        const tags = [];
+        for (let tagName of tagNames) {
+            let classes = "tag tag-icon ";
+            const faClasses = BRAND_FA_CLASSES[tagName.toLowerCase()];
+            //check if we have icon 
+            if(faClasses){
+                //if we have class, add class
+                classes += faClasses;
+            }
+            tags.push(
+                $("<span>", {
+                    class: classes,
+                    text: tagName,
+                    "data-tag-name": tagName
+                })
+            );
+        }
+        $card.find(".card-tags").append(tags);
+        $card.find(".card-body").text(description);
+        $card.find("btn-link").attr("href",url);
+
+        projects.push($card);
+    }
+    $container.append(projects);
+}
+
+$(document).ready(function () {
+
+    // Navigation buttons onClick action
+    $(".page-navs button.btn-nav").click((event) => {
+        const $target = $(event.target);
+        const className = "current";
+        if (!$target.hasClass(className)) {
+            //if button does not have current class, 
+            // change page. 
+            $(".btn-nav.current").removeClass(className);
+            $target.addClass(className);
+            changePage($target.attr("id").split("-")[1]);
+        }
+    });
 });
