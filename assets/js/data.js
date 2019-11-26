67617451220486a1da7aaf4bd092e29c7121c547
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-26 23:03:46 +1100

    update portfolio data and add font awesome classes translator

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,11 +1,41 @@
-const DATA = [
-    {
-        name : "Weather Dashboad",
-        imgURL: "https://raw.githubusercontent.com/cynwong/Weather_Dashboard/master/assets/images/weather_dashboard.png?token=AKKMXC3ZXCSF2NJQ6YFS7UC526OYQ",
-        githubURL: "https://github.com/cynwong/Weather_Dashboard",
-        deployedURL: "https://cynwong.github.io/Weather_Dashboard/",
-        description: "Weather dashboard is an one-page application where the user can search for the weather information by the name of a city.",
-        tags: ["HTML", "HTML5", "CSS", "Bootstrap", "Font-awesome", "JavaScript", "jQuery", "AJAX", "Promise", "OpenWeatherMap API", "API", "LocalStorage", "moment.js"]
+const BRAND_FA_CLASSES= {
+    android: "fab fa-android",
+    angular: "fab fa-angular",
+    bootstrap: "fab fa-bootstrap",
+    css3: "fabfa-css3",
+    "font-awesome": "fab fa-font-awesome",
+    html5: "fab fa-html5",
+    github: "fab fa-github",
+    gitlab: "fab fa-gitlab",
+    javascript: "fab fa-js",
+    markdown: "fab fa-markdown",
+    "node-js": "fab fa-node-js",
+    npm: "fab fa-npm",
+    python: "fab fa-python",
+    "stack-overflow": "fab fa-stack-overflow",
 
-    }
-]
+
+};
+
+const PORTFOLIO_DATA = [
+    {
+        name : "Day Planner",
+        imgAlt: "Day Planner screenshot image",
+        imgURL: "./assets/images/day_planner.png",
+        githubURL: "",
+        deployedURL: "https://cynwong.github.io/day_planner/",
+        description: "Day Planner is a mini project where work day schelduler is created for the user to save their daily tasks.",
+        tags: ["HTML", "HTML5", "CSS", "CSS3", "Bootstrap", "Font-awesome", "JavaScript", "jQuery", "LocalStorage", "moment.js"]
+
+    },
+    {
+        name : "Searchy",
+        imgAlt: "Searchy screenshot image",
+        imgURL: "./assets/images/searchy.png",
+        githubURL: "",
+        deployedURL: "https://cynwong.github.io/day_planner/",
+        description: "Searchy is a group project where I am responsible for rendition of the API's repsonse data into the application data.",
+        tags: ["HTML", "HTML5", "CSS", "CSS3", "Bulma", "JavaScript", "jQuery", "LocalStorage", "moment.js", "AJAX", "Promise", "API", "Open Weather API"]
+
+    },
+];
