16f5b7e27a97f6bb61038fe01b9c5bf6d3a1829c
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-10 12:54:20 +1100

    added btn-menu listener

--- assets/js/on_load.js
+++ assets/js/on_load.js
@@ -2,35 +2,51 @@
 
 $(document).ready(function () {
 
+	// toggle menu button
+	$(".btn-menu").click((event) => {
+		const $btn = $('.btn-menu-burger');
+		const $menu = $(".menu");
+
+		if($btn.hasClass('open')){
+			//close the menu
+			$btn.removeClass('open');
+			$menu.removeClass('open');
+			return null;
+		}
+		// open the menu
+		$btn.addClass('open');
+		$menu.addClass('open');
+	});
+
     // Navigation buttons onClick action
-    $(".page-navs button.btn-nav").click((event) => {
-        const $target = $(event.target);
-        const className = "current";
-        if (!$target.hasClass(className)) {
-            //if button does not have current class, 
-            // change page. 
-            $(".btn-nav.current").removeClass(className);
-            $target.addClass(className);
-            changePage($target.attr("id").split("-")[1]);
-        }
-    });
+    // $(".page-navs button.btn-nav").click((event) => {
+    //     const $target = $(event.target);
+    //     const className = "current";
+    //     if (!$target.hasClass(className)) {
+    //         //if button does not have current class, 
+    //         // change page. 
+    //         $(".btn-nav.current").removeClass(className);
+    //         $target.addClass(className);
+    //         changePage($target.attr("id").split("-")[1]);
+    //     }
+    // });
 
-    $("#projects-container").on("click", ".btn-more", function(event){
-        event.preventDefault();
-        const index = $(event.target).closest(".card-container").data("index");
-        render_details(index);
-    });
+    // $("#projects-container").on("click", ".btn-more", function(event){
+    //     event.preventDefault();
+    //     const index = $(event.target).closest(".card-container").data("index");
+    //     render_details(index);
+    // });
 
-    $("#project-details  .btn-close").click(function(){
-        closeModal();
-    });
-    $("#project-details").click(function(event){
-        const $target = $(event.target);
-        if($target.hasClass("modal")){
-            //if click on empty space, 
-            //close the modal. 
-            closeModal();
-        }
-    });
+    // $("#project-details  .btn-close").click(function(){
+    //     closeModal();
+    // });
+    // $("#project-details").click(function(event){
+    //     const $target = $(event.target);
+    //     if($target.hasClass("modal")){
+    //         //if click on empty space, 
+    //         //close the modal. 
+    //         closeModal();
+    //     }
+    // });
 
 });
