25854006b3468f1e5040476d8867ccc2189241a0
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-06 22:11:28 +1100

    Add Home page

---
+++ dev/src/components/Pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+	return (
+		<section>
+			<h2>Hello. I am </h2>
+			<h1>
+				<span className='first-name'>Cynthia</span> Wong,
+			</h1>
+			<h3>
+				a Full-stack Web Developer.
+			</h3>
+		</section>
+	)
+}
