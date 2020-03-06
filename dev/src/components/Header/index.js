c11f2b34c465846ac1e1973709e45ccd4912851a
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-06 21:43:21 +1100

    add header

---
+++ dev/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+import MenuButton from './MenuButton';
+import NavMenu from './NavMenu';
+import SiteTitle from './SiteTitle';
+
+export default function Header() {
+	const isClient =  typeof window === 'object';
+	const getWidth = () => isClient ? window.innerWidth : undefined;
+
+	const [screenWidth, setScreenWidth] = useState(getWidth());
+
+	useEffect(() => {
+		if(!isClient) return false;
+		const handleResize = () => setScreenWidth(getWidth());
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	console.log(screenWidth);
+	return (
+		<header>
+			<SiteTitle />
+			<MenuButton />
+			{/* <NavMenu /> */}
+		</header>
+	);
+};
