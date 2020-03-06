70fa323e9403d3201ad52167671cd2f008f77b52
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-06 22:09:00 +1100

    Add Router and Route

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
-	<Header />
+	<Router>
+		<Header />
+		<main>
+			<Route exact path='/' component={Home} />
+		</main>
+	</Router>
   );
 }
 
