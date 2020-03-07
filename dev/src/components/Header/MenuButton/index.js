8a33309897e53077ffe0d465cba11d2a09c2bcc1
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-07 15:11:19 +1100

    Add MenuButton component

--- dev/src/components/Header/MenuButton/index.js
+++ dev/src/components/Header/MenuButton/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+// style
+import './styles.scss';
+
 export default function MenuButton() {
+	const handleOnClick = (e) => {
+		e.preventDefault();
+		console.log('button clicked.')
+
+	};
 	return (
-		<div className='btn-menu'>
+		<div className='btn-menu' onClick={handleOnClick}>
 			<span className='btn-menu-burger'></span>
 		</div>
 	)
