186bde0e936d219a4ebc377dbabf9328b2c9fec2
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-07 15:10:20 +1100

    Add site title component

--- dev/src/components/Header/SiteTitle/index.js
+++ dev/src/components/Header/SiteTitle/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+
+//style 
+import './styles.scss';
+
 export default function SiteTitle() {
 	return (
 		<div className='site-title'>
-			<h1 className='site-title'>Cynthia Wong</h1>
+			<h1>Cynthia Wong</h1>
 			<p>Full-Stack Web Developer</p>
 		</div>
 	)
