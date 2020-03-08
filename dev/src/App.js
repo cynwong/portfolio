66235b5ef00e6ea57fd1c06f3596eacdde036d07
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-08 19:19:40 +1100

    Add Footer and about page

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// Component
+import Footer from './components/Footer';
 import Header from './components/Header';
+import About from './components/Pages/About';
 import Home from './components/Pages/Home';
 
+// style
+import './App.scss';
+
+
+
 function App() {
-  return (
-	<Router>
-		<Header />
-		<main>
-			<Route exact path='/' component={Home} />
-		</main>
-	</Router>
-  );
+	return (
+		<Router>
+			<div className='wrapper'>
+				<Header />
+				<main>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/about' component={About} />
+				</main>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
