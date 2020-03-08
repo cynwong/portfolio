2e386cf0717db33c6b273a1b6531652d438c225c
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-09 10:25:21 +1100

    update card

--- dev/src/components/Card/index.js
+++ dev/src/components/Card/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 
+import Tag from '../Tag';
+
 import './styles.scss';
 
 export default function Card(props) {
 	const {
-		title,
+		name,
 		imageUrl,
 		imageCaption,
-		tags,
-		body
-	} = props;
+		mainTags: tags,
+		description
+	} = props.project;
+	console.log(tags)
 	return (
 		<div className='card-container'>
 			<div className='card'>
 				<div className="card-front">
 					<img 
 						className='card-front-image' 
-						src={imageUrl ? imageUrl : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMrgcAATsA3BT31OAAAAAASUVORK5CYII="} 
+						data-src={imageUrl}
+						src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMrgcAATsA3BT31OAAAAAASUVORK5CYII=" 
 						alt={imageCaption}
 					/>
-					<div className="card-front-title">{title}</div>
+					<div className="card-front-title">{name}</div>
 				</div>
 				<div className='card-content'>
-					<h5 className='card-content-title'>{title}</h5>
+					<h5 className='card-content-title'>{name}</h5>
 					<ul className='card-content-tags'>
-						{tags.map((tag) => <Tag label={tag} />)}
+						{tags.map((tag) => <Tag tag={tag} />)}
 					</ul>
-					<p className='card-content-body'>{body}</p>
+					<p className='card-content-body'>{description}</p>
 					<a className='btn btn-more'>Learn More &gt;</a>
 				</div>
 			</div>
