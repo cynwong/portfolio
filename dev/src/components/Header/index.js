917215e0449d0b275039ab34b529ffdc08d3fcc9
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-08 23:26:48 +1100

    modify header to mobile responsive

--- dev/src/components/Header/index.js
+++ dev/src/components/Header/index.js
@@ -2,26 +2,29 @@ import React, { useContext } from 'react';
 
 // components
 import MenuButton from './MenuButton';
-import NavMenu from './NavMenu';
+import NavMenu from '../NavMenu';
 import SiteTitle from './SiteTitle';
 
 // utils
-import ScreenWidthContext from '../../utils/ScreenWidthContext';
+import ScreenWidthContext from '../../utils/AppContext';
 
 // style
 import './styles.scss';
 
 export default function Header() {
-	const screenWidth = useContext(ScreenWidthContext);
+	const { 
+		isMobile,
+	} = useContext(ScreenWidthContext);
+	
 
-	const content = (screenWidth >= 576) ? ( 
+	const content = (isMobile) ? ( 
+		<header className='header'>
+			<MenuButton />
+		</header>
+		) : ( 
 			<header className='header'>
 				<SiteTitle /> 
 				<NavMenu />
-			</header>
-		) : ( 
-			<header className='header'>
-				<MenuButton />
 			</header>
 		);
 	return content;
