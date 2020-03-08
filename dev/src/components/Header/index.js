8230236448f72fd08addadd53286154eac0f3cce
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-08 21:45:00 +1100

    Use ScreenWidthContext API and responsive menu

--- dev/src/components/Header/index.js
+++ dev/src/components/Header/index.js
@@ -1,37 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
+// components
 import MenuButton from './MenuButton';
 import NavMenu from './NavMenu';
 import SiteTitle from './SiteTitle';
+
+// utils
+import ScreenWidthContext from '../../utils/ScreenWidthContext';
 
 // style
 import './styles.scss';
 
 export default function Header() {
-	const isClient =  typeof window === 'object';
-	const getWidth = () => isClient ? window.innerWidth : undefined;
+	const screenWidth = useContext(ScreenWidthContext);
 
-	const [screenWidth, setScreenWidth] = useState(getWidth());
-
-	useEffect(() => {
-		if(!isClient) return false;
-		const handleResize = () => setScreenWidth(getWidth());
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
-
-	console.log(screenWidth);
-	const displaySiteTitle = () => {
-		if (screenWidth >= 576) {
-			return ( <SiteTitle /> );
-		}
-		return '';
-	}
-	return (
-		<header className='header'>
-			<MenuButton />
-			{ displaySiteTitle() }
-			<NavMenu />
-		</header>
-	);
+	const content = (screenWidth >= 576) ? ( 
+			<header className='header'>
+				<SiteTitle /> 
+				<NavMenu />
+			</header>
+		) : ( 
+			<header className='header'>
+				<MenuButton />
+			</header>
+		);
+	return content;
 };
