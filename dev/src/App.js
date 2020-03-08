1193799208089cc20473c22e140554091bcd0e85
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-08 21:05:33 +1100

    add project details and redirections

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { createContext, useState, useEffect }  from 'react';
+import { 
+	BrowserRouter as Router, 
+	Route,
+	Switch,
+	Redirect
+ } from 'react-router-dom';
 
 // Component
 import Footer from './components/Footer';
@@ -8,25 +13,44 @@ import About from './components/Pages/About';
 import Contact from './components/Pages/Contact';
 import Home from './components/Pages/Home';
 import Portfolio from './components/Pages/Portfolio';
+import ProjectDetail from './components/Pages/ProjectDetail';
 
 // style
 import './App.scss';
 
-
-
 function App() {
+	// get Screen width
+	const isClient =  typeof window === 'object';
+	const getWidth = () => isClient ? window.innerWidth : undefined;
+
+	const [screenWidth, setScreenWidth] = useState(getWidth());
+	const ScreenWidthContext = createContext();
+
+	useEffect(() => {
+		if(!isClient) return false;
+		const handleResize = () => setScreenWidth(getWidth());
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
 	return (
 		<Router>
-			<div className='wrapper'>
-				<Header />
-				<main>
-					<Route exact path='/' component={Home} />
-					<Route exact path='/about' component={About} />
-					<Route exact path='/contact' component={Contact} />
-					<Route exact path='/portfolio' component={Portfolio} />
-				</main>
-				<Footer />
-			</div>
+			<ScreenWidthContext.Provider value={screenWidth}>
+				<div className='wrapper'>
+					<Header />
+					<main>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/about' component={About} />
+							<Route exact path='/contact' component={Contact} />
+							<Route exact path='/portfolio' component={Portfolio} />
+							<Route path='/projects/:id' component={ProjectDetail} />
+							<Route path='*'><Redirect to='/' /></Route>
+						</Switch>
+					</main>
+					<Footer />
+				</div>
+			</ScreenWidthContext.Provider>
 		</Router>
 	);
 }
