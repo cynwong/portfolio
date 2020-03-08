77bcf2a7fe4f7300e60fead8f5ae6b1519573150
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-08 23:28:56 +1100

    Change NavMenu for mobile responsiveness

--- dev/src/components/NavMenu/index.js
+++ dev/src/components/NavMenu/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // component
 import NavItem from './NavItem';
 
+// utils
+import AppContext from '../../utils/AppContext';
+
 // style
 import './styles.scss';
 
-export default function index() {
+export default function NavMenu() {
+	const { 
+		openMenu,
+	} = useContext(AppContext);
 	return (
-		<nav className='menu'>
+		<nav className={openMenu? 'menu open': 'menu'}>
 			<ul>
-				<NavItem label='Home' to='/' />
 				<NavItem label='About Me' to='/about' />
 				<NavItem label='My Portfolio' to='/portfolio' />
 				<NavItem label='Contact Me' to='/contact' />
