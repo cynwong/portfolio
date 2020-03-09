835c0feaef946b9a156cac8eb161064af8cfdf08
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-09 21:36:31 +1100

    add nextBtn and prevBtn to project details

--- dev/src/components/Pages/ProjectDetail/index.js
+++ dev/src/components/Pages/ProjectDetail/index.js
@@ -49,53 +49,48 @@ export default function ProjectDetail(props) {
 		}
 	}, [id,mainRef]);
 
-	const onClickHandler = (e) => {
-		e.preventDefault();
-		const xCoord = e.pageX;
-
-		if( xCoord < (screenSize / 4)){
-			// go to previous project
-			return history.push(`/projects/${prevKey}`);
-		} else if ( xCoord > ((screenSize * 3) / 4)){
-			// go to next project
-			return history.push(`/projects/${nextKey}`);
-		}
-	}
-
-	
 	return (
-		<section className="project-details" onClick={onClickHandler}>
-			<div className='project-details-header'>
-				<h2 className='project-details-header-title'>
-					{name}
-				</h2>
+		<section className="project-details">
+			<div 
+				className="prevBtn" 
+				onClick={()=> history.push(`/projects/${prevKey}`)}
+			>
+				&lt;
 			</div>
-			<div className='project-details-body'>
-				<img 
-					className='project-details-body-image' 
-					src={images(imageUrl)}
-					alt={imageCaption} 
-				/>
-				<p className='project-details-body-description'>
-					{description}
-				</p>
-				<ul className='project-details-body-tag-cloud'>
-					{
-						[...mainTags, ...otherTags]
-							.map((tag,index) => <Tag tag={tag} key={index} />)
-					}
-				</ul>
+			<div className="details-content">
+				<div className='details-content-header'>
+					<h2 className='details-content-header-title'>
+						{name}
+					</h2>
+				</div>
+				<div className='details-content-body'>
+					<img 
+						className='details-content-body-image' 
+						src={images(imageUrl)}
+						alt={imageCaption} 
+					/>
+					<p className='details-content-body-description'>
+						{description}
+					</p>
+					<ul className='details-content-body-tag-cloud'>
+						{
+							[...mainTags, ...otherTags]
+								.map((tag,index) => <Tag tag={tag} key={index} />)
+						}
+					</ul>
+				</div>
+				<div className='details-content-footer'>
+					<a className='btn btn-site' href={deployedUrl} target='_blank' rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faExternalLinkAlt} title='Deployed site' />
+						<span className='tooltip right'>Visit site</span>
+					</a>
+					<a className='btn btn-github' href={githubUrl} target='_blank' rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faGithub} title='GitHub' />
+						<span className='tooltip left'>View code</span>
+					</a>
+				</div>
 			</div>
-			<div className='project-details-footer'>
-				<a className='btn btn-site' href={deployedUrl} target='_blank' rel="noopener noreferrer">
-					<FontAwesomeIcon icon={faExternalLinkAlt} title='Deployed site' />
-					<span className='tooltip right'>Visit site</span>
-				</a>
-				<a className='btn btn-github' href={githubUrl} target='_blank' rel="noopener noreferrer">
-					<FontAwesomeIcon icon={faGithub} title='GitHub' />
-					<span className='tooltip left'>View code</span>
-				</a>
-			</div>
+			<div className="nextBtn" onClick={()=> history.push(`/projects/${nextKey}`)}>&gt;</div>
 		</section>
 	)
 }
