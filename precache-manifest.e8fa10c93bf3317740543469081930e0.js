3c79c50044bc3d201a3082fd9f38f7e25e617859
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-09 23:54:25 +1100

    Add built files for production

---
+++ precache-manifest.e8fa10c93bf3317740543469081930e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641fb26770a17b793f773fe1d5b9c153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35debcae0bcab6c4cf45",
+    "url": "/static/css/main.2f57dbf6.chunk.css"
+  },
+  {
+    "revision": "2ecff1ee1239038d6474",
+    "url": "/static/js/2.ce471d2b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ce471d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35debcae0bcab6c4cf45",
+    "url": "/static/js/main.022b444e.chunk.js"
+  },
+  {
+    "revision": "550b02638763e552cf69",
+    "url": "/static/js/runtime-main.edd88c68.js"
+  },
+  {
+    "revision": "822dba40f0e9744dacc5ccbeea4bd32a",
+    "url": "/static/media/Employee Tracker.822dba40.png"
+  },
+  {
+    "revision": "19bfe71f836106abefa4b9c973a123a8",
+    "url": "/static/media/Fitness Tracker.19bfe71f.png"
+  },
+  {
+    "revision": "e025fa0af412bf956ac17c97970aea67",
+    "url": "/static/media/RecipeLover.e025fa0a.png"
+  },
+  {
+    "revision": "6e2d92ce26b971edba4634a0f3939c59",
+    "url": "/static/media/Scheduler.6e2d92ce.png"
+  },
+  {
+    "revision": "b1ee99079ac080cff075e10e90404555",
+    "url": "/static/media/Searchy.b1ee9907.png"
+  },
+  {
+    "revision": "168c119c954a6636d244c99c89f143f5",
+    "url": "/static/media/WeatherDashboard.168c119c.png"
+  },
+  {
+    "revision": "782e60562ba6c03660abf2120b91fd71",
+    "url": "/static/media/background.782e6056.jpg"
+  }
+]);
