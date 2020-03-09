d040af0c371b9f3b7b3e3d105862180ab472692f
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-09 15:52:05 +1100

    Add screensize to AppContext

--- dev/src/utils/AppContext.js
+++ dev/src/utils/AppContext.js
@@ -2,6 +2,7 @@ import { createContext } from 'react'
 
 const AppContext = createContext({
 	isMobile: false,
+	screenSize: 0,
 	showMenu: false,
 	portfolioData: {},
 	onMenuButtonClick: () => undefined
