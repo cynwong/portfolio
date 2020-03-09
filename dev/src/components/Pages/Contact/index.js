2f51f9254a9ddd9a97e5b8cb4b02838d8634e7c9
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-03-10 00:09:48 +1100

    contact page scroll to top  on page load

--- dev/src/components/Pages/Contact/index.js
+++ dev/src/components/Pages/Contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWordpress, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -7,14 +7,28 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 import './styles.scss';
 
+import AppContext from '../../../utils/AppContext';
+
 export default function Contact() {
+	const { mainRef } = useContext(AppContext);
+	useEffect(() => {
+		if (mainRef.current) {
+			mainRef.current.scroll({top: 0, left: 0, behavior: 'smooth' });
+		}
+	}, [mainRef]);
 	return (
 		<section className='contact'>
 				<div className='section-content'>
 					<div className='contact location'>
 						<FontAwesomeIcon icon={faMapMarkerAlt} title='Location' />
 						<span className="colon">:</span> 
-						<a href='mailto:cynthiaswong88@gmail.com'>Melbourne, VIC, Australia</a>
+						<a 
+							href='https://www.google.com/maps/place/Melbourne+VIC/@-37.9701476,144.4913173,9z/data=!3m1!4b1!4m5!3m4!1s0x6ad646b5d2ba4df7:0x4045675218ccd90!8m2!3d-37.8136276!4d144.9630576'
+							target='_blank'
+							rel="noopener noreferrer"
+						>
+							Melbourne, VIC, Australia
+						</a>
 					</div>
 					<div className='contact email'>
 						<FontAwesomeIcon icon={faEnvelope} title='Email' />
@@ -24,17 +38,35 @@ export default function Contact() {
 					<div className='contact linkedIn'>
 						<FontAwesomeIcon icon={faLinkedin} title='LinkedIn' />
 						<span className="colon">:</span> 
-						<a href='https://www.linkedin.com/in/cynthia-wong-29944348/'>www.linkedin.com/in/cynthia-wong-29944348/</a>
+						<a 
+							href='https://www.linkedin.com/in/cynthia-wong-29944348/'
+							target='_blank'
+							rel="noopener noreferrer"
+						>
+							www.linkedin.com/in/cynthia-wong-29944348/
+						</a>
 					</div>
 					<div className='contact github'>
 						<FontAwesomeIcon icon={faGithub} title='GitHub' />
 						<span className="colon">:</span> 
-						<a href='https://github.com/cynwong'>github.com/cynwong</a>
+						<a 
+							href='https://github.com/cynwong'
+							target='_blank'
+							rel="noopener noreferrer"
+						>
+							github.com/cynwong
+						</a>
 					</div>
 					<div className='contact blog'>
 						<FontAwesomeIcon icon={faWordpress} title='Blog' />
 						<span className="colon">:</span> 
-						<a href='https://cynw.wordpress.com'>cynw.wordpress.com</a>
+						<a 
+							href='https://cynw.wordpress.com'
+							target='_blank'
+							rel="noopener noreferrer"
+						>
+							cynw.wordpress.com
+						</a>
 					</div>
 
 					<div id='contact-me'>
