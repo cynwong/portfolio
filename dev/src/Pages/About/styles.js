b34a3a2c1b91aee21c6d91d7c0ae5b4c029a98b5
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-08-05 21:21:13 +1000

    change heading color and add lineheight to p

--- dev/src/Pages/About/styles.js
+++ dev/src/Pages/About/styles.js
@@ -23,12 +23,15 @@ export const Header = styled.h1`
 	margin-bottom: 2rem;
 
 	font-size: 2rem;
-	color: ${Colors.frenchRose};
+	color: ${Colors.deepCerise};
+	text-shadow: 0px 0px 3px ${Colors.paleSky};
 `;
 
 export const Paragraph = styled.p`
 	margin-bottom: 1rem;
+
 	text-align: justify;
+	line-height: 1.25;
 
 	&:last-child {
 		margin-bottom: 3rem;
