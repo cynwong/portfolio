e6865466e883bcaee4bf5399f69b03bfea296209
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-08-05 21:12:37 +1000

    Update about page styles

--- dev/src/Pages/About/index.js
+++ dev/src/Pages/About/index.js
@@ -1,8 +1,14 @@
 import React, { useContext, useEffect } from 'react';
 
-import './styles.scss';
+// import './styles.scss';
 
 import AppContext from '../../utils/AppContext';
+
+import {
+	Section,
+	Header,
+	Paragraph
+} from './styles';
 
 export default function About() {
 	const { mainRef } = useContext(AppContext);
@@ -14,21 +20,23 @@ export default function About() {
 	}, [mainRef]);
 
 	return (
-		<section className='about'>
-				<div className='section-content'>
-					<h1>Who is Cynthia?</h1>
-					<p>
-						Cynthia is a Full-stack Web Developer, based in Melbourne, Australia.
-					</p>
-					<p>
-						Along many twists and turns of her career path, she picked up numerous soft skills such as research
-						skills, teamwork, coaching, management, quick adaptability to new environment, working closely with others, multitasking among other things.  Although she enjoyed working in other fields, her
-						passion has always been for the Technology.
-					</p>
-					<p>
-						Since she returned back to the IT field, she fell in love with the coding all over again. &nbsp;As a web developer, she loves utilizing her love for challenges, competitive spirit, insatiable intellectual curiosity, attention to details, and goal-oriented work-ethic to develop innovative and seamless applications. &nbsp;She is looking forward to bringing her diverse arsenal of both technical and soft-skills to any development team to enhance their productivity.
-					</p>
-				</div>
-			</section>
+		<Section>
+			<Header>Who Am I?</Header>
+			<Paragraph>
+				{
+					`Cynthia is a Full-stack Web Developer, based in Melbourne, Australia.`
+				}
+			</Paragraph>
+			<Paragraph>
+			{
+				`Along many twists and turns of her career path, she picked up numerous soft skills such as research skills, teamwork, coaching, management, quick adaptability to new environment, working closely with others, multitasking among other things.  Although she enjoyed working in other fields, her passion has always been for the Technology.`
+			}
+			</Paragraph>
+			<Paragraph>
+			{
+				`Since she returned back to the IT field, she fell in love with the coding all over again.  As a web developer, she loves utilizing her love for challenges, competitive spirit, insatiable intellectual curiosity, attention to details, and goal-oriented work-ethic to develop innovative and seamless applications.  She is looking forward to bringing her diverse arsenal of both technical and soft-skills to any development team to enhance their productivity.`
+			}
+			</Paragraph>
+		</Section>
 	)
 }
---
+++ dev/src/Pages/About/styles.js
@@ -0,0 +1,62 @@
+
+import styled from 'styled-components';
+import { rgba } from 'polished';
+
+import { Colors, pillCorner, borderRadius } from '../../Components/App/styles';
+
+export const Section = styled.section`
+	max-width: 90%;
+
+	padding: 1rem 1.5rem;
+
+	position: absolute;
+    top: 20%;
+    left: 5%;
+	right: 5%;
+
+	border-radius: ${borderRadius};
+	
+	background: ${rgba(Colors.blackPearl, 0.75)};
+`;
+
+export const Header = styled.h1`
+	margin-bottom: 2rem;
+
+	font-size: 2rem;
+	color: ${Colors.frenchRose};
+`;
+
+export const Paragraph = styled.p`
+	margin-bottom: 1rem;
+	text-align: justify;
+
+	&:last-child {
+		margin-bottom: 3rem;
+	}
+`;
+
+export const Button = styled.button`
+	&& {
+		display: block;
+
+		width: 9rem;
+		margin: 3rem auto;
+		padding: 1rem;
+
+		border-radius: ${pillCorner};
+		border: 1px solid ${Colors.snowDrift};
+		
+		color: ${Colors.snowDrift};
+		text-align: center;
+
+		transition: all 1s ease-in-out;
+		
+		&:hover, &:focus, &:active {
+			background-color: ${Colors.frenchRose};
+
+			border-color: ${Colors.frenchRose};
+
+			transform: scale(1.2);
+		}
+	}
+`;
