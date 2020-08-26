09f73c100d131904d71568ee03d835a689f86b5b
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-08-26 10:26:09 +1000

    Move style constants to under utils

--- dev/src/components/App/Styles.js
+++ dev/src/components/App/Styles.js
@@ -3,38 +3,6 @@ import { rgba } from 'polished';
 
 import image from '../../images/background.jpg';
 
-export const Colors = {
-	blackPearl: '#101217',
-	smoky: '#636365',
-	frenchRose: '#f141a0',
-	deepCerise: '#d84293',
-	snowDrift: '#e9e9e7',
-
-	// border colors
-	cinder: '#272c35',
-	paleSky: '#59686f',
-	
-	// tag colors
-	celeste: '#d7d7c3', // background
-	verdunGreen: '#584f1b', //api
-	tyrianPurple: '#680138', // js
-	amazon: '#387058', // stylesheet
-	goldenBrown: '#a15c17', // template-engine
-	persianBlue: '#1c40d3', // frontend
-	jewel: '#13660c', //testing
-};
-
-export const borderRadius = '10px';
-
-export const pillCorner = '50px';
-
-export const breakpoints = {
-	xs: '350px',
-	sm: '576px',
-	md: '768px',
-	lg: '992px',
-	xl: '1200px'
-}
 
 export const GlobalStyles = createGlobalStyle`
 	@font-face {
